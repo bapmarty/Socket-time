e3da0cbe2579dac45a7f5c2cf42c3991f118d790
bapmarty <contact@baptistemrt.fr>
2021-03-12 16:05:19 +0000

    :rocket: Change deploy stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:5001";
+const ENDPOINT = "https://stime.baptistemrt.fr";
 
 function App() {
   const [response, setResponse] = useState("");
